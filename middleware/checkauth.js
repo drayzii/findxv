08351374cdf3a7c17fee98cb5fa8d7e6312f7cab
drayzii <50402526+drayzii@users.noreply.github.com>
2019-07-27 14:35:39 +0200

    X

--- middleware/checkauth.js
+++ middleware/checkauth.js
@@ -1,5 +1,5 @@
 const check = (req,res,next)=>{
-if(req.body.email=='fraterne01@gmail.com'&&req.body.password=='fraterne123458'){
+if(req.body.email==='fraterne01@gmail.com'&&req.body.password==='fraterne123458'){
     next()
 } else{
     res.status(403).json({
